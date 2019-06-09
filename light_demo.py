8e43fd318e9d13d615de46976a14f586fa7e9457
Joshua Riddell <jridd30@gmail.com>
2019-06-09 02:39:27 +0100

    Add light demo

---
+++ light_demo.py
@@ -0,0 +1,20 @@
+# import the GPIO module
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# set which pin number we used for the light
+light_red = 4
+
+# setup the pin as an output
+GPIO.setup(light_red, GPIO.OUT)
+
+# turn the light on
+GPIO.output(light_red, True)
+
+# wait for 3 seconds
+time.sleep(3)
+
+# turn the light off
+GPIO.output(light_red, False)
